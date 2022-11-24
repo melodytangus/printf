f8d8f1e64576751336a3e48497326b12dc9b693a
Erickkaranja <karanja99erick@gmail.com>
2022-11-24 19:09:07 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -21,7 +21,6 @@ void cleanup(va_list args, buffer_t *output)
  * @format: Character string to print - may contain directives.
  * @output: A buffer_t struct containing a buffer.
  * @args: A va_list of arguments.
- *
  * Return: The number of characters stored to output.
  */
 int run_printf(const char *format, va_list args, buffer_t *output)
